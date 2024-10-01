649771bc9a46546dd1e2e57a6a37aebbfa0a9c7e
DinerBoll <43893582+DinerBoll@users.noreply.github.com>
2024-10-01 16:45:53 +0300

    Create homework5.py

---
+++ homework5.py
@@ -0,0 +1,17 @@
+my_list = ['orange', 'apple', 'melon', 'strawberry', 'watermelon', 'banana', 'pear', 'grape']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'kiwi'
+print(my_list)
+
+my_dict = {'a' : 'First letter of the alphabet',
+           'z' : 'Last letter of the alphabet',
+           1 : 'Number',
+           'Nikita' : 'my name'
+           }
+print(my_dict)
+print(my_dict['a'])
+my_dict['Age'] = 19
+my_dict['Nikita'] = 'Not my name'
+print(my_dict)
