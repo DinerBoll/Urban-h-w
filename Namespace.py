7c68a5c1d102eef852e6caccebe53f4c3519cb5f
DinerBoll <43893582+DinerBoll@users.noreply.github.com>
2024-12-07 00:41:07 +0300

    Update Namespace.py

--- Namespace.py
+++ Namespace.py
@@ -1,7 +1,9 @@
 def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
-        
+
+    # Вызов inner_function внутри test_function
     inner_function()
 
+# Вызов test_function
 test_function()
